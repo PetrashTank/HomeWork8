abd126a794075ea596b49f423d65b48f8325ee73
Tatiana <zanka_16@mail.ru>
2023-02-10 12:43:19 +0800

    Задание 3 ДЗ8

---
+++ Task3/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+Console.WriteLine("Введите размерность первой матрицы: ");
+int[,] A = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+FillArray(A);
+Console.WriteLine("Введите размерность второй матрицы: ");
+int[,] B = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+FillArray(B);
+
+if (A.GetLength(1) != B.GetLength(0) || A.GetLength(0) != B.GetLength(1))
+{
+    Console.Write("Матрицы нельзя перемножить");
+}
+else
+{
+    Console.WriteLine("\nМатрица C = A * B:");
+    int[,] C = MultArray(A, B);
+    PrintArray(C);
+}
+
+void FillArray(int[,] array)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(0, 10);
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine(" ");
+    }
+}
+
+static int[,] MultArray(int[,] a, int[,] b)
+{
+    int[,] r = new int[a.GetLength(0), b.GetLength(1)];
+
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < b.GetLength(1); j++)
+        {
+            for (int k = 0; k < b.GetLength(0); k++)
+            {
+                r[i, j] += a[i, k] * b[k, j];
+            }
+        }
+    }
+    return r;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
